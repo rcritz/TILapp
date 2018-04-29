f803b1ebb4df9506b6bdc088923d374132839d14
Richard Critz <rcritz@gmail.com>
2018-04-29 19:29:41 -0400

    Add leaf templates

--- Package.swift
+++ Package.swift
@@ -4,12 +4,20 @@ import PackageDescription
 let package = Package(
   name: "TILApp",
   dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
-    .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
+    .package(url: "https://github.com/vapor/vapor.git",
+             from: "3.0.0-rc"),
+    .package(
+      url: "https://github.com/vapor/fluent-postgresql.git",
+      from: "1.0.0-rc"),
+    .package(url: "https://github.com/vapor/leaf.git",
+             from: "3.0.0-rc")
   ],
   targets: [
-    .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+    .target(name: "App",
+            dependencies: ["FluentPostgreSQL",
+                           "Vapor",
+                           "Leaf"]),
     .target(name: "Run", dependencies: ["App"]),
     .testTarget(name: "AppTests", dependencies: ["App"]),
-  ]
+    ]
 )
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -47,4 +47,7 @@ public func routes(_ router: Router) throws {
 
   let categoriesController = CategoriesController()
   try router.register(collection: categoriesController)
+
+  let websiteController = WebsiteController()
+  try router.register(collection: websiteController)
 }
